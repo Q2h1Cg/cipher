e7796cf3bcb4ec7233bf70fc7d92beb9d5707c38
Chu <root@sh3ll.me>
2015-11-28 11:03:24 +0800

    prob3

---
+++ solutions/prob3-readme.txt
@@ -0,0 +1,9 @@
+Single-byte XOR cipher
+题目描述:题目中给出了一个经过单字节数据异或加密而成的16进制字符串，要求解出密钥以及明文。
+例如：
+The hex encoded string:
+1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736
+结果：
+potential key: 0x58
+Potential hex(plain): 436f6f6b696e67204d432773206c696b65206120706f756e64206f66206261636f6e
+potential plaintext: "Cooking MC's like a pound of bacon"
---
+++ solutions/prob3.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# Written against python 3.3.1
+# Matasano Problem 3
+# The hex encoded string:
+#  1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736
+# has been XOR'd against a single character. Find the key, decrypt
+# the message.
+
+from prob1 import hexToRaw, rawToHexLUT
+from prob2 import hex_xor
+import string
+
+letterFrequency = {}
+letterFrequency['A'] = .082;
+letterFrequency['B'] = .015;
+letterFrequency['C'] = .028;
+letterFrequency['D'] = .043;
+letterFrequency['E'] = .127;
+letterFrequency['F'] = .022;
+letterFrequency['G'] = .020;
+letterFrequency['H'] = .061;
+letterFrequency['I'] = .070;
+letterFrequency['J'] = .002;
+letterFrequency['K'] = .008;
+letterFrequency['L'] = .040;
+letterFrequency['M'] = .024;
+letterFrequency['N'] = .067;
+letterFrequency['O'] = .075;
+letterFrequency['P'] = .019;
+letterFrequency['Q'] = .001;
+letterFrequency['R'] = .060;
+letterFrequency['S'] = .063;
+letterFrequency['T'] = .091;
+letterFrequency['U'] = .028;
+letterFrequency['V'] = .010;
+letterFrequency['W'] = .023;
+letterFrequency['X'] = .001;
+letterFrequency['Y'] = .020;
+letterFrequency['Z'] = .001;
+letterFrequency[' '] = .200;
+
+# calculate score
+def calculateMG(plain):
+    counts = [];
+    for i in range(256):
+        counts.append(0);
+    
+    for i in range(len(plain)):
+        if (plain[i] < 128):
+            counts[ord(chr(plain[i]).upper())] += 1;    
+    
+    result = 0.0;
+    for i in string.ascii_uppercase:
+        result += letterFrequency[i]*counts[ord(i)];
+    result += letterFrequency[' '] * counts[ord(' ')];
+    result /= len(plain);
+    return result;
+
+def tryKey(cipher, key):
+    fullkey = key * len(cipher);
+    fullkey = fullkey[:len(cipher)];
+    potential_plain = hex_xor(cipher, fullkey);
+    return calculateMG(hexToRaw(potential_plain)), potential_plain;
+
+# brute force every key (1,256)
+def findGoodKeys(cipher):
+    for i in range(256):
+        mg, plain = tryKey(cipher, rawToHexLUT[i]);
+        if (mg > .050):
+            print("potential key: 0x" + rawToHexLUT[i]);
+            print("Potential hex(plain): " + str(plain).lstrip("b'").rstrip("'"));
+            print("potential plaintext: " + str(hexToRaw(str(plain).lstrip("b'").rstrip("'"))).lstrip("b'").rstrip("'"));
+        
+        
+if __name__ == "__main__":
+    cip = b'1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736';
+    findGoodKeys(cip);
